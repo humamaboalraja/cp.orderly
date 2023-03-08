b6e4748493359857730cc9c0ebf6d79ba50216e2
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-08 16:19:12 +0100

    test(common:domain): Enable test coverage

---
+++ common/test-coverage-report/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("kotlin-tests-conventions")
+}
+
+tasks {
+    jacocoTestReport {
+        dependsOn(
+            ":common:domain:test",
+        )
+        val domainCommonUtils = file("../domain/build/test.exec")
+
+        executionData.setFrom(listOf(domainCommonUtils))
+        sourceDirectories.setFrom(
+            project(":common:domain").sourceSets.getByName("main").output
+
+        )
+        classDirectories.setFrom(
+            project(":common:domain").sourceSets.getByName("main").allSource
+        )
+        additionalClassDirs.setFrom(
+            project(":common:domain").sourceSets.getByName("main").output
+        )
+        additionalSourceDirs.setFrom(
+            project(":common:domain").sourceSets.getByName("main").allSource
+
+        )
+        sourceSets(
+            project(":order:domain:core").sourceSets.getByName("main"),
+        )
+    }
+}
