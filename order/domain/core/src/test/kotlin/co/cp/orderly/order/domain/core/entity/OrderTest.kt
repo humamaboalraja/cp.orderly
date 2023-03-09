85615fc707237862c47a3d3983c5fdb7bb5798bb
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-09 01:16:22 +0100

    test(order:domain): Add `Order` state machine & helper utils tests

--- order/domain/core/src/test/kotlin/co/cp/orderly/order/domain/core/entity/OrderTest.kt
+++ order/domain/core/src/test/kotlin/co/cp/orderly/order/domain/core/entity/OrderTest.kt
@@ -272,5 +272,229 @@ class OrderTest {
         )
     }
 
+    @Test
+    fun `should change the orderStatus using pay() order state machine method`() {
+        order.pay()
+        assertEquals(order.orderStatus, OrderStatus.PAID)
+        order.orderStatus = OrderStatus.APPROVED
+        val payStateException = { order.pay() }
+        val payStateExceptionResult = assertThrows<OrderDomainException>(payStateException)
+        assertEquals(payStateExceptionResult.message, "Order is not in valid state for a pay operation")
+    }
 
+    @Test
+    fun `should change the orderStatus using approve() order state machine method`() {
+        order.orderStatus = OrderStatus.PAID
+        order.approve()
+        assertEquals(order.orderStatus, OrderStatus.APPROVED)
+        order.orderStatus = OrderStatus.APPROVED
+        val approveStateException = { order.approve() }
+        val approveStateExceptionResult = assertThrows<OrderDomainException>(approveStateException)
+        assertEquals(
+            approveStateExceptionResult.message,
+            "Order is not in valid state for an approve operation"
+        )
+    }
+
+    @Test
+    fun `should change the orderStatus using initCancel(failureMessages) order state machine method`() {
+        order.orderStatus = OrderStatus.PAID
+        order.initCancel(order.failureMessages)
+        assertEquals(order.orderStatus, OrderStatus.CANCELLING)
+        order.orderStatus = OrderStatus.APPROVED
+        val initCancelStateException = { order.initCancel(order.failureMessages) }
+        val initCancelStateExceptionResult = assertThrows<OrderDomainException>(initCancelStateException)
+        assertEquals(
+            initCancelStateExceptionResult.message,
+            "Order is not in a valid state for an initCancel operation"
+        )
+    }
+
+    @Test
+    fun `should change the orderStatus using cancel(failureMessages) order state machine method`() {
+        order.orderStatus = OrderStatus.CANCELLING
+        order.cancel(order.failureMessages)
+        assertEquals(order.orderStatus, OrderStatus.CANCELED)
+        order.orderStatus = OrderStatus.PENDING
+        order.cancel(order.failureMessages)
+        assertEquals(order.orderStatus, OrderStatus.CANCELED)
+        order.orderStatus = OrderStatus.APPROVED
+        val cancelStateException = { order.cancel(order.failureMessages) }
+        val cancelStateExceptionResult = assertThrows<OrderDomainException>(cancelStateException)
+        assertEquals(
+            cancelStateExceptionResult.message,
+            "Order is not in a valid state for a cancel operation"
+        )
+    }
+
+    @Test
+    fun `should call startNewtOrder() and verify the state changes`() {
+        order.startNewtOrder()
+        assertEquals(order.orderStatus, OrderStatus.PENDING)
+        assertDoesNotThrow { UUID.fromString(order.orderId?.getValue().toString()) }
+        assertDoesNotThrow { UUID.fromString(order.trackingId?.getValue().toString()) }
+    }
+
+    @Test
+    fun `should call validateItemsPrice() and verify its internal methods call`() {
+        val exceptionData = assertThrows<OrderDomainException> { orderSecond.validateOrder() }
+        assertEquals(
+            exceptionData.message,
+            "The total price must be greater than 0"
+        )
+    }
+
+    @Test
+    fun `should call validateInitialOrder() and verify its cases`() {
+        val exceptionData = assertThrows<OrderDomainException> { orderThird.validateOrder() }
+        assertEquals(
+            exceptionData.message,
+            "Order is not in a correct state for initialization"
+        )
+    }
+
+    @Test
+    fun `should call validateOrder() and verify its internal methods calls & their cases`() {
+
+        assertDoesNotThrow { }
+        var exceptionData = assertThrows<OrderDomainException> { orderFourth.build().validateOrder() }
+        exceptionData.message?.contains("Order item price: 5325 is not valid for product: ")?.let { assertTrue(it) }
+        orderFourth.price(null).build()
+        exceptionData = assertThrows { orderFourth.build().validateOrder() }
+        assertEquals(exceptionData.message, "The total price must be greater than 0")
+
+        orderFourth.orderStatus(OrderStatus.PENDING).orderId(OrderId(UUID.randomUUID())).build()
+        exceptionData = assertThrows { orderFourth.build().validateOrder() }
+        assertEquals(exceptionData.message, "Order is not in a correct state for initialization")
+
+        exceptionData = assertThrows {
+            orderFourth
+                .orderStatus(null)
+                .orderId(null)
+                .price(Money(BigDecimal(9)))
+                .items(
+                    listOf(
+                        OrderItem.builder()
+                            .orderItemId(OrderItemId(3124253676))
+                            .orderId(OrderId(UUID.randomUUID()))
+                            .product(
+                                Product(
+                                    ProductId(UUID.fromString("9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6")),
+                                    "dsfgnh",
+                                    Money(BigDecimal(53285))
+                                )
+                            )
+                            .quantity(8)
+                            .price(Money(BigDecimal(5325)))
+                            .subTotal(Money(BigDecimal(108650)))
+                            .build()
+                    )
+                )
+                .build().validateOrder()
+        }
+        assertEquals(
+            exceptionData.message,
+            "Order item price: 5325 is not valid for product: 9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6"
+        )
+
+        assertDoesNotThrow {
+            orderFourth
+                .orderStatus(null)
+                .orderId(null)
+                .price(Money(BigDecimal(106570)))
+                .items(
+                    listOf(
+                        OrderItem.builder()
+                            .orderItemId(OrderItemId(3124253676))
+                            .orderId(OrderId(UUID.randomUUID()))
+                            .product(
+                                Product(
+                                    ProductId(UUID.fromString("9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6")),
+                                    "dsfgnh",
+                                    Money(BigDecimal(53285))
+                                )
+                            )
+                            .quantity(2)
+                            .price(Money(BigDecimal(53285)))
+                            .subTotal(Money(BigDecimal(106570)))
+                            .build()
+                    )
+                )
+                .build().validateOrder()
+        }
+
+        val orderFourthObject = orderFourth
+            .orderStatus(null)
+            .orderId(null)
+            .price(Money(BigDecimal(106570)))
+            .items(
+                listOf(
+                    OrderItem.builder()
+                        .orderItemId(OrderItemId(3124253676))
+                        .orderId(OrderId(UUID.randomUUID()))
+                        .product(
+                            Product(
+                                ProductId(UUID.fromString("9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6")),
+                                "dsfgnh",
+                                Money(BigDecimal(53285))
+                            )
+                        )
+                        .quantity(2)
+                        .price(Money(BigDecimal(53285)))
+                        .subTotal(Money(BigDecimal(106570)))
+                        .build()
+                )
+            ).failureMessages(mutableListOf("sfgdfg", "rwagsdfdg", "fsgdfd"))
+            .orderStatus(OrderStatus.PENDING)
+        orderFourthObject.build().cancel(mutableListOf("exception", "exception #1", "exception #2"))
+
+        assertEquals(
+            orderFourthObject.failureMessages,
+            mutableListOf("sfgdfg", "rwagsdfdg", "fsgdfd", "exception", "exception #1", "exception #2")
+        )
+
+        val orderFourthO2ndObject = orderFourth
+
+        orderFourthO2ndObject.failureMessages(null).build().cancel(mutableListOf("exception", "exception #1", "exception #2"))
+        assertEquals(orderFourthO2ndObject.failureMessages, mutableListOf("sfgdfg", "rwagsdfdg", "fsgdfd", "exception", "exception #1", "exception #2", "exception", "exception #1", "exception #2"))
+
+        val orderFourthO3rdObject = orderFourth
+        orderFourthO3rdObject.items(null).build().startNewtOrder()
+        assertEquals(orderFourthO3rdObject.items, null)
+
+        var order5th = orderFourth
+
+        order5th.price(Money(BigDecimal(1065790)))
+            .items(
+                listOf(
+                    OrderItem.builder()
+                        .orderItemId(OrderItemId(3124253676))
+                        .orderId(OrderId(UUID.randomUUID()))
+                        .product(
+                            Product(
+                                ProductId(UUID.fromString("9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6")),
+                                "dsfgnh",
+                                Money(BigDecimal(53285))
+                            )
+                        )
+                        .quantity(2)
+                        .price(Money(BigDecimal(53285)))
+                        .subTotal(Money(BigDecimal(106570)))
+                        .build()
+                )
+            )
+
+        val exceptionDataTwo = assertThrows<OrderDomainException> { order5th.build().validateItemsPrice() }
+        assertEquals(exceptionDataTwo.message, "Total Price: 1065790 doesn't match the order items total price 106570.00")
+    }
+
+    @Test
+    fun `Should call order's toString() method and verify its returned value`() {
+        assertTrue(!order.toString().isNullOrEmpty() && order.toString().startsWith("Order", true))
+        listOf(
+            "failureMessages", "orderId", "customerId", "shopId", "price", "items", "orderStatus", "failureMessages"
+        ).forEach {
+            assertTrue(order.toString().contains(it))
+        }
+    }
 }
