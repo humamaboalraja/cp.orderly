cda149282940ffd00332f96b47324332faf97bca
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-09 01:11:11 +0100

    feat(order:domain): Add `Order` state machine & helper utils

--- order/domain/core/src/main/kotlin/co/cp/orderly/order/domain/core/entity/Order.kt
+++ order/domain/core/src/main/kotlin/co/cp/orderly/order/domain/core/entity/Order.kt
@@ -6,8 +6,12 @@ import co.cp.orderly.domain.vos.Money
 import co.cp.orderly.domain.vos.OrderId
 import co.cp.orderly.domain.vos.OrderStatus
 import co.cp.orderly.domain.vos.ShopId
+import co.cp.orderly.order.domain.core.exception.OrderDomainException
+import co.cp.orderly.order.domain.core.vos.OrderItemId
 import co.cp.orderly.order.domain.core.vos.StreetAddress
 import co.cp.orderly.order.domain.core.vos.TrackingId
+import java.math.BigDecimal
+import java.util.UUID
 
 class Order private constructor(
     val orderId: OrderId?,
@@ -49,6 +53,109 @@ class Order private constructor(
         fun build() = Order(
             orderId, customerId, shopId, deliveryAddress, price, items, trackingId, orderStatus, failureMessages
         )
+    }
+
+    fun startNewtOrder() {
+        setId(OrderId(UUID.randomUUID()))
+        trackingId = TrackingId(UUID.randomUUID())
+        orderStatus = OrderStatus.PENDING
+        initializeOrderItems()
+    }
+
+    private fun changeOrderState(
+        operationCondition: () -> Boolean,
+        exceptionMessage: String,
+        operation: () -> Unit
+    ) = when {
+        operationCondition() -> throw OrderDomainException(exceptionMessage)
+        else -> Unit
+    }.also { operation() }
+
+    fun pay() = changeOrderState(
+        operationCondition = { orderStatus != OrderStatus.PENDING },
+        exceptionMessage = "Order is not in valid state for a pay operation",
+        operation = { orderStatus = OrderStatus.PAID }
+    )
+
+    fun approve() = changeOrderState(
+        operationCondition = { orderStatus != OrderStatus.PAID },
+        exceptionMessage = "Order is not in valid state for an approve operation",
+        operation = { orderStatus = OrderStatus.APPROVED }
+    )
+
+    fun initCancel(failureMessages: MutableList<String>) = changeOrderState(
+        operationCondition = { orderStatus != OrderStatus.PAID },
+        exceptionMessage = "Order is not in a valid state for an initCancel operation",
+        operation = {
+            orderStatus = OrderStatus.CANCELLING
+            updateFailureMessages(failureMessages)
+        }
+    )
+
+    fun cancel(failureMessages: MutableList<String>) = changeOrderState(
+        operationCondition = { !(orderStatus == OrderStatus.PENDING || orderStatus == OrderStatus.CANCELLING) },
+        exceptionMessage = "Order is not in a valid state for a cancel operation",
+        operation = {
+            orderStatus = OrderStatus.CANCELED
+            updateFailureMessages(failureMessages)
+        }
+    )
+
+    private fun updateFailureMessages(failureMessages: MutableList<String>) {
+        when {
+            failureMessages != null && this.failureMessages != null ->
+                this.failureMessages?.addAll(failureMessages.filter { message -> message.isNotEmpty() }.toMutableList())
+            this.failureMessages == null -> this.failureMessages = failureMessages
+        }
+    }
+
+    fun validateOrder() {
+        validateInitialOrder()
+        validateTotalPrice()
+        validateItemsPrice()
+    }
+
+    fun validateItemsPrice() = items?.map { orderItem ->
+        validateItemPrice(orderItem)
+        orderItem.subTotal
+    }?.fold(Money.ZERO_VALUE) {
+        acc, money ->
+        money?.addMoney(acc) ?: Money(BigDecimal(0))
+    }.also {
+        when {
+            price?.getAmount()?.toInt() != it?.getAmount()?.toInt() -> throw OrderDomainException(
+                "Total Price: ${price?.getAmount()} doesn't match the order items total price ${it?.getAmount()}"
+            )
+            else -> Unit
+        }
+    }
+
+    private fun validateItemPrice(orderItem: OrderItem) = when {
+        !orderItem.isPriceValid() ->
+            throw OrderDomainException(
+                "Order item price: ${orderItem.price?.getAmount()} " +
+                    "is not valid for product: ${orderItem.product?.getId()?.getValue()}"
+            )
+        else -> Unit
+    }
+
+    private fun validateTotalPrice() = when {
+        price == null || !price.isAmountGreaterThanZero() ->
+            throw OrderDomainException("The total price must be greater than 0")
+        else -> Unit
+    }
+
+    private fun validateInitialOrder() = when {
+        orderStatus != null || getId() != null ->
+            throw OrderDomainException("Order is not in a correct state for initialization")
+        else -> Unit
+    }
+
+    private fun initializeOrderItems() {
+        var itemId: Long = 1
+        items?.forEach {
+            it.initializeOrderItem(super.getId(), OrderItemId(itemId++))
+        }
     }
 
     override fun toString(): String =
