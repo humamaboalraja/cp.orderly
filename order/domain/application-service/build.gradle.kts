cf2c2deb1accffe620b34575f1801b08efdec2e9
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-12 20:00:58 +0100

    build: Add common:domain, and other external dependencies

--- order/domain/application-service/build.gradle.kts
+++ order/domain/application-service/build.gradle.kts
@@ -1,7 +1,14 @@
+val springBootVersion: String by project
+
 plugins {
     id("spring-app-module-conventions")
+    kotlin("plugin.lombok") version "1.8.10"
+    id("io.freefair.lombok") version "5.3.0"
 }
 
 dependencies {
     api(project(":order:domain:core"))
+    api(project(":common:domain"))
+    implementation("org.springframework:spring-tx:6.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation:$springBootVersion")
 }
