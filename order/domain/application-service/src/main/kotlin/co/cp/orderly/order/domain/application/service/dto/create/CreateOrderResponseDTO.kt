25fc9280909e8d4c03c59fd8c0e2d9dce83b5c80
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-13 13:53:53 +0100

    feat: Add Order service's repositories interfaces for adapters to implement

--- order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/dto/create/CreateOrderResponseDTO.kt
+++ order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/dto/create/CreateOrderResponseDTO.kt
@@ -4,9 +4,6 @@ import co.cp.orderly.domain.vos.OrderStatus
 import java.util.UUID
 import javax.validation.constraints.NotNull
 
-// @AllArgsConstructor
-// @Builder
-// @Getter
 data class CreateOrderResponseDTO(
     @NotNull
     private val orderTrackingId: UUID?,
