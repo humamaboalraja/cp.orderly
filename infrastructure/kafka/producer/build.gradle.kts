2cda8d878452af9b8e2b8ae04cf41fe8fcd08672
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-22 19:24:39 +0100

    build(infrastructure): Setup `Kafka` Cosumer and Producer module structure

---
+++ infrastructure/kafka/producer/build.gradle.kts
@@ -0,0 +1,28 @@
+val springBootVersion: String by project
+val kafkaAvroSerializer: String by project
+plugins {
+    id("kotlin-tests-conventions")
+    id("spring-conventions")
+}
+
+// Overwriting project default repositories
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://packages.confluent.io/maven")
+    }
+}
+
+dependencies {
+    api(project(":infrastructure:kafka:config"))
+    api(project(":infrastructure:kafka:model"))
+    api(project(":order:domain:core"))
+    api(project(":order:domain:core"))
+    api(project(":common:domain"))
+    implementation("org.springframework.kafka:spring-kafka:$springBootVersion")
+    implementation("io.confluent:kafka-avro-serializer:$kafkaAvroSerializer") {
+        exclude(group = "org.slf4j")
+        exclude(group = "io.swagger")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-validation:$springBootVersion")
+}
