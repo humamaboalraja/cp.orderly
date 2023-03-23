29df09a6893695d17b80c8ddff4a20365f36fac9
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-23 18:25:57 +0100

    build: Setup Order `Application module buils logic

--- order/application/build.gradle.kts
+++ order/application/build.gradle.kts
@@ -1,7 +1,12 @@
+val springBootVersion: String by project
+
 plugins {
     id("spring-app-module-conventions")
 }
 
 dependencies {
-    project(":order:domain:application-service")
+    api(project(":common:application"))
+    api(project(":order:domain:application-service"))
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-validation:$springBootVersion")
 }
