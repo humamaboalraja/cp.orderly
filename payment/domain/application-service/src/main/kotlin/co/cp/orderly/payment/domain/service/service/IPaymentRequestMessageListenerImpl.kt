ab464175482dad9becd609f2bf52003efcd08483
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-31 14:52:35 +0200

    feat(payment): Add `PaymentRequestMessageListenerImpl` implemetaion

---
+++ payment/domain/application-service/src/main/kotlin/co/cp/orderly/payment/domain/service/service/IPaymentRequestMessageListenerImpl.kt
@@ -0,0 +1,40 @@
+package co.cp.orderly.payment.domain.service.service
+
+import co.cp.orderly.payment.domain.core.events.PaymentEvent
+import co.cp.orderly.payment.domain.service.dto.PaymentServiceRequestDTO
+import co.cp.orderly.payment.domain.service.ports.input.message.listener.IPaymentRequestMessageListener
+import co.cp.orderly.payment.domain.service.ports.outptut.message.publisher.IPaymentCanceledMessagePublisher
+import co.cp.orderly.payment.domain.service.ports.outptut.message.publisher.IPaymentCompletedMessagePublisher
+import co.cp.orderly.payment.domain.service.ports.outptut.message.publisher.IPaymentFailedMessagePublisher
+import co.cp.orderly.payment.domain.service.service.helper.PaymentApplicationServiceRequestHelper
+import org.springframework.stereotype.Service
+import java.util.logging.Logger
+
+@Service
+class PaymentRequestMessageListenerImpl(
+    private val paymentRequestHelper: PaymentApplicationServiceRequestHelper,
+    private val paymentCompletedMessagePublisher: IPaymentCompletedMessagePublisher,
+    private val paymentCanceledMessagePublisher: IPaymentCanceledMessagePublisher,
+    private val paymentFailedMessagePublisher: IPaymentFailedMessagePublisher
+) : IPaymentRequestMessageListener {
+
+    companion object { private val logger = Logger.getLogger(PaymentRequestMessageListenerImpl::class.java.name) }
+
+    private fun fireEvent(paymentEvent: PaymentEvent) {
+        logger.info(
+            "Publishing Payment event for Order #${paymentEvent.payment.orderId?.getValue()} - " +
+                "Payment #${paymentEvent.payment.getId()?.getValue()}"
+        )
+        paymentEvent.fire()
+    }
+
+    override fun completePayment(paymentRequest: PaymentServiceRequestDTO) {
+        val paymentEvent = paymentRequestHelper.persistPayment(paymentRequest)
+        fireEvent(paymentEvent)
+    }
+
+    override fun cancelPayment(paymentRequest: PaymentServiceRequestDTO) {
+        val paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest)
+        fireEvent(paymentEvent)
+    }
+}
