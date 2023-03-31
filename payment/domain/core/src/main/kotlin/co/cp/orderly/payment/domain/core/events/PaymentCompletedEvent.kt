d6780ac3ade8fd46361f0e3f3fb360a2905fe5a5
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-03-31 14:20:26 +0200

    feat: Create `Payment` domain core `events` & `exceptions`

---
+++ payment/domain/core/src/main/kotlin/co/cp/orderly/payment/domain/core/events/PaymentCompletedEvent.kt
@@ -0,0 +1,14 @@
+package co.cp.orderly.payment.domain.core.events
+
+import co.cp.orderly.domain.event.publisher.DomainEventPublisher
+import co.cp.orderly.payment.domain.core.entity.Payment
+import java.time.ZonedDateTime
+
+class PaymentCompletedEvent(
+    payment: Payment,
+    createdAt: ZonedDateTime,
+    errorMessages: MutableList<String>? = null,
+    private val paymentCompletedDomainEventPublisher: DomainEventPublisher<PaymentCompletedEvent>
+) : PaymentEvent(payment, createdAt, mutableListOf()) {
+    override fun fire() = run { paymentCompletedDomainEventPublisher.publish(this) }
+}
---
+++ payment/domain/core/src/main/kotlin/co/cp/orderly/payment/domain/core/exception/PaymentNotFoundException.kt
@@ -0,0 +1,8 @@
+package co.cp.orderly.payment.domain.core.exception
+
+import co.cp.orderly.domain.exception.DomainException
+
+class PaymentNotFoundException : DomainException {
+    constructor(message: String) : super(message)
+    constructor(message: String?, cause: Throwable) : super(message, cause)
+}
