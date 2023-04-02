40a83a2abfa74458a9dd873a09129db15fb5348c
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-02 22:39:54 +0200

    chore: Add application config and db schema initializer

---
+++ payment/main/src/main/resources/database/seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO orderly.payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('37e06f7a-e5b6-4172-b139-a845bef9b869', 'ab4ffa49-bcdc-4a03-a20b-3ae611a4bffc', 492.00);
+INSERT INTO orderly.payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('00e047dc-6839-4e3b-a3dd-9bc1150036df', '9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6', 142.00);
+INSERT INTO orderly.payment.credit_history(id, customer_id, amount, type)
+VALUES ('f66b7626-79b7-44d2-b01f-48611914a1d9', 'ab4ffa49-bcdc-4a03-a20b-3ae611a4bffc', 188.00, 'CREDIT');
+INSERT INTO orderly.payment.credit_history(id, customer_id, amount, type)
+VALUES ('81872c0e-95fe-4294-83aa-36fe90f68d94', 'ab4ffa49-bcdc-4a03-a20b-3ae611a4bffc', 748.00, 'CREDIT');
+INSERT INTO orderly.payment.credit_history(id, customer_id, amount, type)
+VALUES ('fd0efdf9-c2ff-4aae-9306-41d3a909e999', '9fb3673d-2bfb-4e1b-a0b7-a0f2bd7098e6', 163.00, 'CREDIT');
+INSERT INTO orderly.payment.credit_history(id, customer_id, amount, type)
+VALUES ('969a47d0-289a-4af9-9b2c-6d50d75e98f9', 'ab4ffa49-bcdc-4a03-a20b-3ae611a4bffc', 321.00, 'DEBIT');
+
+
+
+
