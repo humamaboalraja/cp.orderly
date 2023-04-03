ab799e3f39fb91aa4deccb1bebfe48e9b161e024
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-03 23:55:33 +0200

    feat(shop): Add order data persistece repository

---
+++ shop/data/src/main/kotlin/co/cp/orderly/shop/data/repository/OrderApprovalPersistenceRepository.kt
@@ -0,0 +1,11 @@
+package co.cp.orderly.shop.data.repository
+
+import co.cp.orderly.shop.data.entity.OrderApprovalEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface OrderApprovalPersistenceRepository : JpaRepository<OrderApprovalEntity, UUID> {
+
+}
