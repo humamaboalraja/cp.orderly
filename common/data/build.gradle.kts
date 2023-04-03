89bf41c33809c94acd89bbc9992669aebb0304f4
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-03 23:46:11 +0200

    build(shop): Setup data module

--- common/data/build.gradle.kts
+++ common/data/build.gradle.kts
@@ -1,13 +1,10 @@
 val springBootVersion: String by project
 val postgresqlClientVersion: String by project
-postgresqlClientVersion
+
 plugins {
     id("spring-app-module-conventions")
 }
 
 dependencies {
-    api(project(":order:domain:application-service"))
-    api(project(":common:data"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
-    implementation("org.postgresql:postgresql:$postgresqlClientVersion")
-}
+    implementation("org.postgresql:postgresql:$postgresqlClientVersion")}
