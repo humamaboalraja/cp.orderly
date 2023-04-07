2189388e66ad3082fee7dd569a3987dab5649846
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-07 21:38:06 +0200

    ref: Remove `container` builder task

--- customer/main/build.gradle.kts
+++ customer/main/build.gradle.kts
@@ -2,7 +2,8 @@
 val springBootVersion: String by project
 val postgresqlClientVersion: String by project
 
-plugins { id("spring-app-webapp-conventions")
+plugins {
+    id("spring-app-webapp-conventions")
 }
 
 configurations {
@@ -16,10 +17,4 @@ dependencies {
 //    api(project(":customer:messaging"))
     implementation("org.springframework.boot:spring-boot-starter:$springBootVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
-
 }
-
-//tasks.register<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
-//    imageName.set("${project.group}/order.service:${project.version}")
-//    archiveFile.set(File("./build/libs/main.jar"))
-//}
