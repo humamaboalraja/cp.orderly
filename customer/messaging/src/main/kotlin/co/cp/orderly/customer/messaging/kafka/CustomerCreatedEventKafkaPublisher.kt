b20c6a772938d65b71cfe1a4fce60ef4722c7b84
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-09 21:34:14 +0200

    fix(CS): Omit breaking Kafka model types

--- customer/messaging/src/main/kotlin/co/cp/orderly/customer/messaging/kafka/CustomerCreatedEventKafkaPublisher.kt
+++ customer/messaging/src/main/kotlin/co/cp/orderly/customer/messaging/kafka/CustomerCreatedEventKafkaPublisher.kt
@@ -2,12 +2,9 @@ package co.cp.orderly.customer.messaging.kafka
 
 import co.cp.orderly.customer.domain.core.event.CustomerCreatedEvent
 import co.cp.orderly.customer.messaging.mapper.CustomerMessagingDataMapper
-import co.cp.orderly.kafka.model.avro.CustomerAvroModel
 import cp.cp.orderly.customer.domain.application.service.config.CustomerApplicationServiceConfig
 import cp.cp.orderly.customer.domain.application.service.ports.output.messaging.ICustomerMessagePublisher
-import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
-import org.springframework.util.concurrent.ListenableFutureCallback
 import java.util.logging.Logger
 
 @Component
@@ -24,13 +21,6 @@ class CustomerCreatedEventKafkaPublisher(
             "Received CustomerCreatedEvent for customer #${customerCreatedEvent.customer.getId()?.getValue()}"
         )
         try {
-            // val customerAvroModel = customerMessagingDataMapper
-            // .paymentResponseAvroModelToPaymentResponse(customerCreatedEvent)
-            // kafkaProducer.send(
-            // customerApplicationServiceConfig.customerTopicName, customerAvroModel.id,
-            // customerAvroModel,
-            // getCallback(customerApplicationServiceConfig.customerTopicName, customerAvroModel)
-            // )
             logger.info(
                 "CustomerCreatedEvent sent to kafka for customer #customerAvroModel.id",
 
@@ -41,29 +31,6 @@ class CustomerCreatedEventKafkaPublisher(
                     "for customer #${customerCreatedEvent.customer.getId()?.getValue()}," +
                     " error: {e.message}",
             )
-        }
-    }
-
-    private fun getCallback(
-        topicName: String,
-        message: CustomerAvroModel
-    ): ListenableFutureCallback<SendResult<String, CustomerAvroModel>> {
-        return object : ListenableFutureCallback<SendResult<String, CustomerAvroModel>> {
-            override fun onFailure(throwable: Throwable) {
-                logger.info(
-                    "Error while sending message $message to topic $topicName" +
-                        "exception: $throwable"
-                )
-            }
-
-            override fun onSuccess(result: SendResult<String, CustomerAvroModel>?) {
-                val metadata = result?.getRecordMetadata()
-                logger.info(
-                    "Received new metadata. Topic: ${metadata?.topic()}; Partition ${metadata?.partition()}; " +
-                        "Offset ${metadata?.offset()}; Timestamp ${metadata?.timestamp()}, " +
-                        "at ${System.nanoTime()}",
-                )
-            }
         }
     }
 }
