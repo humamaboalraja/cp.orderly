5b6aadc328c953b4b8161be777312fcae9a2ebf6
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-14 17:37:08 +0200

    ref(order): Refactor core domain service DTO's

---
+++ order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/dto/message/customer/CustomerModelDTO.kt
@@ -0,0 +1,9 @@
+package co.cp.orderly.order.domain.application.service.dto.message.customer
+
+data class CustomerModelDTO(
+    val id: String,
+    val username: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String
+)
--- order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/dto/track/order/TrackOrderQueryDTO.kt
+++ order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/dto/track/order/TrackOrderQueryDTO.kt
@@ -1,15 +1,10 @@
 package co.cp.orderly.order.domain.application.service.dto.track.order
 
 import jakarta.validation.constraints.NotNull
-import java.math.BigDecimal
 import java.util.UUID
 
 data class TrackOrderQueryDTO(
     @NotNull
     val orderTrackingId: UUID,
-    @NotNull
-    val shopId: UUID? = null,
-    @NotNull
-    val price: BigDecimal? = null,
 
 )
