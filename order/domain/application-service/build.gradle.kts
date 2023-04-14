5454d99aaf8d66dfaecb0e528ce87da06c56d79b
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-14 17:40:14 +0200

    build(order): Omit hard domain service hard coded dependency versions

--- order/domain/application-service/build.gradle.kts
+++ order/domain/application-service/build.gradle.kts
@@ -5,9 +5,13 @@ plugins {
 }
 
 dependencies {
+    api(project(":infrastructure:transactions:consistency"))
+    api(project(":infrastructure:transactions:llt"))
     api(project(":order:domain:core"))
     api(project(":common:domain"))
     implementation("org.springframework:spring-tx:6.0.0")
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.0.5")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.5")
+    implementation("org.springframework.boot:spring-boot-starter-validation:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-json:$springBootVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
+    testImplementation("org.mockito:mockito-core:5.2.0")
 }
