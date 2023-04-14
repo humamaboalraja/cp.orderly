7a8cdd63984a6861bbe901f40bd62b0de9cc5b12
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-14 17:24:52 +0200

    feat(order): Refine order service `messaging` interfaces

--- order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/ports/output/message/publisher/payment/PaymentRequestMessagePublisher.kt
+++ order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/ports/output/message/publisher/payment/PaymentRequestMessagePublisher.kt
@@ -1,6 +1,14 @@
 package co.cp.orderly.order.domain.application.service.ports.output.message.publisher.payment
 
+import ConsistencyState
 import co.cp.orderly.domain.event.publisher.DomainEventPublisher
+import co.cp.orderly.order.domain.application.service.consistency.model.payment.OrderPaymentConsistencyMessage
 import co.cp.orderly.order.domain.core.event.OrderCancelledEvent
+import java.util.function.BiConsumer
 
-interface OrderCancelledPaymentRequestMessagePublisher : DomainEventPublisher<OrderCancelledEvent>
+interface PaymentRequestMessagePublisher : DomainEventPublisher<OrderCancelledEvent> {
+    fun publish(
+        orderPaymentOutboxMessage: OrderPaymentConsistencyMessage,
+        consistencyCallback: BiConsumer<OrderPaymentConsistencyMessage, ConsistencyState>
+    )
+}
