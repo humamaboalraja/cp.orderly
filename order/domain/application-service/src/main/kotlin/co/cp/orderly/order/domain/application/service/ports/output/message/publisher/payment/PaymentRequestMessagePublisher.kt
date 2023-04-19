fc8f457957a8c4f7db7d9774fd5a7b0011fff628
Humam Abo Alraja <humamaboalraja@gmail.com>
2023-04-19 21:20:48 +0200

    ref(OS): clean up domain core service `message publisher` interfaces

--- order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/ports/output/message/publisher/payment/PaymentRequestMessagePublisher.kt
+++ order/domain/application-service/src/main/kotlin/co/cp/orderly/order/domain/application/service/ports/output/message/publisher/payment/PaymentRequestMessagePublisher.kt
@@ -1,12 +1,10 @@
 package co.cp.orderly.order.domain.application.service.ports.output.message.publisher.payment
 
 import ConsistencyState
-import co.cp.orderly.domain.event.publisher.DomainEventPublisher
 import co.cp.orderly.order.domain.application.service.consistency.model.payment.OrderPaymentConsistencyMessage
-import co.cp.orderly.order.domain.core.event.OrderCancelledEvent
 import java.util.function.BiConsumer
 
-interface PaymentRequestMessagePublisher : DomainEventPublisher<OrderCancelledEvent> {
+interface PaymentRequestMessagePublisher {
     fun publish(
         orderPaymentConsistencyMessage: OrderPaymentConsistencyMessage,
         consistencyCallback: BiConsumer<OrderPaymentConsistencyMessage, ConsistencyState>
